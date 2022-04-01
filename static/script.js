0d85796675cff30eff639c03733a2bce2d335b39
haroldcelisnavarrete <harold.celis-navarrete@warwick.ac.uk>
2022-04-01 15:42:58 +0100

    Update script.js

--- static/script.js
+++ static/script.js
@@ -10,7 +10,7 @@ let shuffledQuestions, currentQuestionIndex
 /* this section was added from this webpage: https://www.youtube.com/watch?v=XF1_MlZ5l6M&ab_channel=WebDevSimplified
 to see if i can record what stundets answers*/
 btn.addEventListener("click", e => {
-  console.log(correct)
+  console.log(btn)
 })
 
 
