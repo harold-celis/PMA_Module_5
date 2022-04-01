6b3952a6b926e85438a85c4db6041fe192c9af0b
haroldcelisnavarrete <harold.celis-navarrete@warwick.ac.uk>
2022-04-01 15:36:37 +0100

    Update script.js

--- static/script.js
+++ static/script.js
@@ -3,7 +3,7 @@ const nextButton = document.getElementById('next-btn')
 const questionContainerElement = document.getElementById('question-container')
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
-/*const btn = document.querySelector(".btn")*/
+const btn = document.querySelector(".btn")
 
 let shuffledQuestions, currentQuestionIndex
 
